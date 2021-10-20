94844f3133f82dd55db9cc43a9c9f6be89ef0bee
Rasmus Møller <rasm937k@stud.kea.dk>
2021-10-21 00:00:17 +0200

    mandatory I done

---
+++ 01_Mandatory/app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html")
+})
+
+app.get("/theory", (req, res) => {
+    res.sendFile(__dirname + "/public/theory.html")
+})
+
+app.get("/terminal", (req, res) => {
+    res.sendFile(__dirname + "/public/terminal.html")
+})
+
+app.get("/snippets", (req, res) => {
+    res.sendFile(__dirname + "/public/snippets.html")
+})
+
+const PORT = process.env.PORT || 8080;
+
+const server = app.listen(PORT, (error) => {
+    if (error) {
+        console.log(error);
+    }
+    console.log("Server is running on port", server.address().port);
+});
